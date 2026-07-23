280df0629ad4c1bec556067c7fc089006ba1779c
divyanshu <divyanshusah44@gmail.com>
2026-07-24 02:00:27 +0530

    GET and POST working

--- src/app.js
+++ src/app.js
@@ -1,12 +1,19 @@
 const express = require("express")
 const app = express()
+const notes = []
 
+app.use(express.json())
 
-app.get("/",(req,res)=>{
-    res.send("yo yt")
+app.post("/notes",(req,res)=>{
+    notes.push(req.body)
+
+    res.status(201).json({message:"notes created sucessfully"})
+
 })
 
-
+app.get("/notes", (req,res)=>{
+    res.status(200).json({notes:notes, message:"notes fetched sucessfully"})
+})
 
 
 
