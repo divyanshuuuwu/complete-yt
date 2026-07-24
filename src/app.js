9deb2972388ecee02c12bd16a47d25697fad6a5b
divyanshu <divyanshusah44@gmail.com>
2026-07-24 19:33:00 +0530

    patch working

--- src/app.js
+++ src/app.js
@@ -25,6 +25,31 @@ app.get("/notes", async (req , res)=>{
     })
 })
 
+app.delete("/notes/:id", async(req , res)=>{
+    const id = req.params.id
+
+    await noteModel.findOneAndDelete({
+        _id: id
+    })
+    res.status(200).json({
+        message:"deleted"
+    })
+})
+
+app.patch("/notes/:id", async(req,res)=>{
+    const id = req.params.id
+    const newtitle = req.body.title
+    await noteModel.findByIdAndUpdate(
+        {_id:id},
+        {title:newtitle}
+
+    )
+
+    res.status(200).json({
+        message:"updated sucessfully"
+    })
+})
+
 
 
 
