34d68e39a6e4efe88d159c578f886305d56e1461
divyanshu <divyanshusah44@gmail.com>
2026-07-24 19:08:01 +0530

    get  post working

--- src/app.js
+++ src/app.js
@@ -1,7 +1,29 @@
 const express = require("express")
 const app = express()
-
+const noteModel = require("./models/note.model")
 app.use(express.json())
+
+app.post("/notes", async (req , res)=>{
+    const data = req.body
+    await noteModel.create({
+        title: data.title,
+        description: data.description
+    })
+
+    res.status(201).json({
+        mesaage:"note created sucessfully"
+    })
+})
+
+app.get("/notes", async (req , res)=>{
+
+   const notes = await noteModel.find()
+     res.status(200).json({
+        notes:notes,
+        message:"fetched successfully"
+         
+    })
+})
 
 
 
